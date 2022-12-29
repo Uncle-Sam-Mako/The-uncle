45338bf8f2ca3879662532cab360d13fd7c405dd
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2022-12-29 14:37:44 +0200

    Add & stylize the sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,12 @@
+import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () => {
-    return <>Hello</>
+    return <>
+        <Sidebar></Sidebar>
+    </>
 }
+
 
 export default Layout;
 
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,53 @@
+import './index.scss';
+import Logo from '../../assets/images/logo.png';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub, faTwitter, faFacebook} from '@fortawesome/free-brands-svg-icons'
+const Sidebar = () => {
+    return (
+        <div className="nav-bar">
+            <Link className="logo" to='/'>
+                <img src={Logo} alt="logo" />
+                <p className="sub-logo">Uncle Sam</p>
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                     <FontAwesomeIcon icon={faHome} color="#4d4d4e" ></FontAwesomeIcon>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                     <FontAwesomeIcon icon={faUser} color="#4d4d4e" ></FontAwesomeIcon>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" ></FontAwesomeIcon>
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel='noreferrer' href="https://www.google.com">
+                        <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href="https://www.google.com">
+                        <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href="https://www.google.com">
+                        <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href="https://www.google.com">
+                        <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+
+export default Sidebar;
