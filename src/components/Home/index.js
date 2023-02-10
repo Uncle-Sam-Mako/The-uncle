0281b9d431d73852f9536626819c20c0146e9356
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2023-02-10 13:52:53 +0200

    Style correction : Adjustment for all screens

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -42,9 +42,7 @@ const Home = () => {
                         <img src={homeImg} alt="My portfolio image" />
                     </div>
                 </div>
-                <div className="animated-logo-container">
-                    <AnimatedLogo></AnimatedLogo>
-                </div>
+                
             </div>
             <Loader type="pacman"/>
         </>
