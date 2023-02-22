6d4d5c3d132ad6b6de1c846d1c541252fdf4a39a
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2023-02-22 17:31:39 +0200

    Update home : Delete unused importation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,11 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
 import Loader from 'react-loaders'
-import homeImg from '../../assets/images/home_img.png'
+
 
 
 
 const Home = () => {
-    // State hook to store the value of letterClass, which can be passed to a styled component as a prop
-    // const [letterClass, setLetterClass] = useState('text-animate');
-
-    // Arrays to hold the values needed to render each letter of text.
-    // const nameArray = ['U','n','c','l','e',' ', 'S','a','m'];
-    // const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r'];
-
-    // Run the setTimeout function to update letterClass with the value 'text-animate-hover'
-    // This change will allow the component/text to interact with the hover animation 
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setLetterClass('text-animate-hover') ;
-    //     }, 4000)
-    // })
     
     return (
         <>
