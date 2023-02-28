153bb0cce67e976bdc998df915996980436e572e
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2023-02-28 08:01:56 +0200

    Update Webpack : define process global variable to fix "process not defined" error

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 const webpack = require('webpack');
 
 
 
-module.exports = {
+module.exports = (env, argv) => {
+
+  const MODE = argv.mode || 'development';
+
+  return{
   output: {
     path: path.join(__dirname, "/dist"), // the bundle output path
     filename: "bundle.js", // the name of the bundle
@@ -17,10 +24,27 @@ module.exports = {
     new webpack.ProvidePlugin({ // <-- this plugin makes React available as a global variable, so you don't need to import it in every file  
       React : 'react',
     }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-    
+    new webpack.DefinePlugin({ 
+      'process.env': { 
+          NODE_ENV: JSON.stringify('production') 
+      }, 
+      'process': { // define process global variable 
+          env: { NODE_ENV: JSON.stringify('production') } 
+      } 
+    }),
+    new WorkboxPlugin.GenerateSW({
+        // these options encourage the ServiceWorkers to get in there fast
+        // and not allow any straggling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true,
+        maximumFileSizeToCacheInBytes: 5000000
+    }),
+    new CopyWebpackPlugin({
+      patterns : [
+      { from: 'public/images', to: 'images' },
+      { from: 'public/manifest', to: 'manifest' }
+    ]}),
+    new webpack.HotModuleReplacementPlugin()
   ],
   devServer: {
     port: 3030, // you can change the port
@@ -48,4 +72,6 @@ module.exports = {
       },
     ],
   },
+  
 };
+}
